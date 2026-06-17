f73c35ce73d7ab16f7a57da6b909ebeeb81f525a
Dylanfoc <144268338+Dylanfoc2@users.noreply.github.com>
2026-06-17 14:47:53 +0200

    Add Variables.lua to store player and game info

---
+++ Modules/Variables.lua
@@ -0,0 +1,8 @@
+return = {
+	Hwid = gethwid(),
+	Name = game.Players.LocalPlayer.Name,
+	UserId = game.Players.LocalPlayer.UserId,
+	PlaceId = game.PlaceId,
+	JobId = game.JobId,
+	GameId = game.GameId,
+}
