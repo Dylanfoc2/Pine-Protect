4599d142c927c2bdb5f1b615c853fb88e8d685a7
Dylanfoc <144268338+Dylanfoc2@users.noreply.github.com>
2026-06-17 21:05:51 +0200

    Add ClientId and Executor to Variables module

--- Modules/Variables.lua
+++ Modules/Variables.lua
@@ -1,8 +1,10 @@
 return {
 	Hwid = gethwid(),
+	ClientId = game:GetService("RbxAnalyticsService"):GetClientId(),
 	Name = game.Players.LocalPlayer.Name,
 	UserId = game.Players.LocalPlayer.UserId,
 	PlaceId = game.PlaceId,
 	JobId = game.JobId,
 	GameId = game.GameId,
+	Executor = identifyexecutor(),
 }
